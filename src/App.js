a8c7a13c840217aebb74dbc4781f635efbcb208a
Tushar Ismail <gwalchmei@gmail.com>
2021-04-04 14:30:00 +0800

    Completed all videos in lesson 3. Now on to lesson 4! Yay... also practicing git workflow simultaneously.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import restaurant from "../src/restaurant.jpg"
+
+function Header(props){
+  // console.log(props);
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  // console.log(props);
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around</p>
+      <img src={restaurant} height= {200} alt="napking and silverware at a restaurant table"/>
+      <ul style = {{textAlign: "left"}}>
+        {props.dishes.map((dish) => (
+        <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  // console.log(props);
+  return (
+    <footer>
+      <p>Copyright {props.year} </p>
+    </footer>
+  );
+}
+
+const dishes = [
+  "Mac and Cheese",
+  "Salmon",
+  "Tofu and Veggies",
+  "Minestrone"
+];
+
+const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish}));
+console.log(dishObjects);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Tushar" />
+      <Main adjective = "amazing" dishes={dishObjects} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./App"
 
-ReactDOM.render(
-  React.createElement("h1", {style: {color: "blur"}}, "Heyyyyyy Everyone!"),
-  document.getElementById('root')
-);
+
+ReactDOM.render( <App />, document.getElementById('root'));
 
 
